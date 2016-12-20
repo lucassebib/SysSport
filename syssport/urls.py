cce092f2e22b49cf4358236ac95719d43f3c6b85
lucassebib <lucas.sebib@gmail.com>
2016-12-20 16:38:39 -0300

    Agregue url a index cree vista para index modifique index inicial

--- syssport/urls.py
+++ syssport/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from novedades.views import *
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'syssport.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^index$', pagina_principal),
     url(r'^admin/', include(admin.site.urls)),
 )
