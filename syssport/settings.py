651f64cf4b6ad2bcb6da050a06b55f86a299f83b
Veronica <verooo_bo@hotmail.com>
2017-01-24 22:31:31 -0300

    agregue que en los menues quede seleccionado la pagina actual activa

--- syssport/settings.py
+++ syssport/settings.py
@@ -58,6 +58,17 @@ WSGI_APPLICATION = 'syssport.wsgi.application'
 
 TEMPLATE_DIRS = ('templates',)
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+"django.contrib.auth.context_processors.auth",
+"django.core.context_processors.debug",
+"django.core.context_processors.i18n",
+"django.core.context_processors.media",
+"django.core.context_processors.static",
+"django.core.context_processors.tz",
+"django.contrib.messages.context_processors.messages",
+"django.core.context_processors.request",
+)
+
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_ROOT = '/media/'
