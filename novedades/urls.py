c0259dfb38e750a9cf9284c9715bcb763db7cc2a
lucassebib <lucas.sebib@gmail.com>
2017-01-27 18:20:08 -0300

    se agrego funcionalidad de mostar novedades visible para todos

--- novedades/urls.py
+++ novedades/urls.py
@@ -15,5 +15,7 @@ urlpatterns = patterns('',
     url(r'^novedades/(?P<pk>[0-9]+)/$', DetallesNovedades.as_view(), name='detalle-novedad'),
     url(r'^novedades/crear/$', CrearNovedades.as_view(), name='crear-novedad'),
     url(r'^novedades/(?P<pk>[0-9]+)/modificar/$', ActualizarNovedades.as_view(), name='actualizar-novedad'),
-    url(r'^novedades/(?P<pk>[0-9]+)/eliminar/$', EliminarNovedades.as_view(), name='eliminar-novedad'), 	
+    url(r'^novedades/(?P<pk>[0-9]+)/eliminar/$', EliminarNovedades.as_view(), name='eliminar-novedad'), 
+
+    url(r'^novedades_todos$', ver_novedades_visibilidadTodos, name='ver_novedades_visibilidadTodos'),	
 )
