1feb3cef86db2323386f76cfceddf3de442e4c5f
Veronica <verooo_bo@hotmail.com>
2017-02-11 17:56:06 -0300

    menu login estilizado

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -2,8 +2,9 @@ from django import forms
 from models import Alumno, Direccion, ContactoDeUrgencia
 
 class FormularioAutenticacion(forms.Form):
-	username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'usuario'}))
-	password = forms.CharField(widget=forms.PasswordInput(render_value=False, attrs={'placeholder': 'Password'}))
+	
+	username = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder':'usuario', 'class':'form-control', 'required' :'True'}))
+	password = forms.CharField(label='', widget=forms.PasswordInput(render_value=False, attrs={'placeholder': 'contrasena','class':'form-control', 'required':'True'}))
 
 class FormularioDireccion(forms.ModelForm):
 	class Meta:
