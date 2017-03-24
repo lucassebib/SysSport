384ec5359bcdc17e013fc8ac054cc586f5df5dbb
Lore <Lore>
2017-03-24 14:06:52 -0300

    boton ver mas ver menos en datos medicos

--- syssport/settings/base.py
+++ syssport/settings/base.py
@@ -58,35 +58,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 "django.core.context_processors.request",
 )
 
-#MEDIA_ROOT = '/media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-
-MEDIA_URL = '/media/'
-
-STATICFILES_DIRS = ( 
-    os.path.join(BASE_DIR,'media'),
-    
-)
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'sqlserver_ado',
-        'NAME': 'syssport',
-        'HOST': 'localhost',
-        'USER': 'proyecto',
-        'PASSWORD': 'proyecto2016',
-
-        'OPTIONS': {
-                    'uncicode_result': 'True',
-                    'provider': 'SQLNCLI11',
-                    'use_mars': 'DataTypeCompatibility=80;MARS Connection=True;'
-        }
-    }
-}
-
+ROOT_URLCONF = 'syssport.urls'
 
 WSGI_APPLICATION = 'syssport.wsgi.application'
 
